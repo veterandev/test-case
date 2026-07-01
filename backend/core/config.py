171f6f85540c9ed22e8bd9c475b923e4e78744f6
F. Kalantari <f.kalantari@gmail.com>
2026-07-01 15:46:46 +0330

    load by domain

--- backend/core/config.py
+++ backend/core/config.py
@@ -22,6 +22,8 @@ extra_origins = os.getenv("EXTRA_CORS_ORIGINS", "").strip()
 extra_origins_list = [o.strip() for o in extra_origins.split(",") if o.strip()]
 
 allowed_origins = [
+    "https://casedepth.sis-eg.com:3000",
+    "http://casedepth.sis-eg.com:3000",
     "http://45.129.38.84:3000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
